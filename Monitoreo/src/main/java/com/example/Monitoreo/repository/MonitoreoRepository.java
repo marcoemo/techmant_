80e859dcdc3b7094f72ef0198be3474961ef1553
GiulianoLopresti <gi.lopresti@duocuc.cl>
2025-05-24 16:56:40 -0400

    Microservicio Monitoreo - Guiliano

---
+++ Monitoreo/src/main/java/com/example/Monitoreo/repository/MonitoreoRepository.java
@@ -0,0 +1,8 @@
+package com.example.Monitoreo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.Monitoreo.model.Monitoreo;
+
+public interface MonitoreoRepository extends JpaRepository<Monitoreo, Long> {
+
+}
