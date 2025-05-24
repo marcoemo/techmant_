d9cf153af01dd173f941e28d017bea11eac075cb
Lowo0028 <lo.ormeno@duocuc.cl>
2025-05-24 16:24:00 -0400

    Se me olvido poner el nombre

--- Seguimiento/src/main/java/com/example/Seguimiento/repository/SeguimientoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.Seguimiento.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.Seguimiento.model.Seguimiento;
-
-public interface SeguimientoRepository extends JpaRepository<Seguimiento, Long> {
-}
