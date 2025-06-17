b77ca55ba0b1cc297c54ea6097784bb471075598
GiulianoLopresti <gi.lopresti@duocuc.cl>
2025-06-16 22:55:04 -0400

    Elimine monitoreo

--- Monitoreo/src/main/java/com/example/Monitoreo/repository/MonitoreoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.Monitoreo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.Monitoreo.model.Monitoreo;
-
-public interface MonitoreoRepository extends JpaRepository<Monitoreo, Long> {
-
-}
